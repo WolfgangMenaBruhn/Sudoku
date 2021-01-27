c6fe120e4fbddbb68461e6931633c76e8fcafc23
Wolfgang Mena Bruhn <w.mena@infors-ht.com>
2021-01-27 12:57:19 +0100

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Sudoku")]
+[assembly: AssemblyProduct("Sudoku")]
+[assembly: AssemblyDescription("Sudoku")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Recommended: move all attributes below to a shared solution-wide assembly info named SolutionAssemblyInfo.cs
+
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Sudoku.Views
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
